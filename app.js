7704953f7a2881dfc4423e87acc45bf38e1882e4
brunarafaela03 <bruna.pereira03@escola.pr.gov.br>
2025-05-27 14:51:56 +0000

    ok

---
+++ app.js
@@ -0,0 +1,19 @@
+ function criaCartao(categoria, pergunta, resposta) {
+        let container = document.getElementById('container')
+        let cartao= document.createElement('article')
+        cartao.className='cartao'
+        cartao.innerHTML=` <div class="cartao__conteudo">
+                    <h3>Programação</h3>
+                    <div class="cartao__conteudo__pergunta">
+                       <p> O que é JS?</p> 
+                    </div>
+                    <div class="cartao__conteudo__resposta">
+                        <p>liguagem de programaçao</p>
+                    </div>
+                </div>
+                `
+                container.appendChild(cartao)
+
+  
+       
+}
